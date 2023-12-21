4a01e3fb54df646d4c1153714a3e4ce7538b20b6
vicky0718 <vicky187kvs@gmail.com>
2023-12-21 16:16:24 +0530

    initial commit

---
+++ components/jobdetails/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import { React, useState } from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+
+import styles from "./footer.style";
+import { icons } from "../../../constants";
+
+const Footer = ({ url }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.likeBtn}
+        onPress={() => setIsLiked(!isLiked)}
+      >
+        <Image
+          source={isLiked ? icons.heart : icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Footer;
